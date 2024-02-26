4050cadf85423f3691be1a64e6a7b0f4c8c0c1a1
caveat-dev <nathan.wignall06@gmail.com>
2024-02-25 19:02:06 -0600

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+function navbar() {
+  document.getElementById("navbar").innerHTML = "" +
+    "<ul>\n" +
+    "  <li><a href='../index.html'>Home</a></li>" +
+    "  <li><a href='../pages/about.html'>About</a></li>" +
+    "  <li><a href='../pages/code.html'>Code</a></li>" +
+    "  <li><a href='../pages/games.html'>Games</a></li>" +
+    "  <li><a href='../pages/contact.html'>Contact</a></li>" +
+    "</ul>"
+}
