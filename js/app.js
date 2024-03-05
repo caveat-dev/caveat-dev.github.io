2d2ad006edb2ebca7e1c53af67033e69e931e886
caveat-dev <nathan.wignall06@gmail.com>
2024-03-05 15:37:15 -0600

    Delete everything

--- js/app.js
+++
@@ -1,9 +0,0 @@
-function navbar() {
-  document.getElementById("navbar").innerHTML = "" +
-    "<ul>\n" +
-    "  <li><a href='../pages/about.html'>About</a></li>" +
-    "  <li><a href='../pages/code.html'>Code</a></li>" +
-    "  <li><a href='../pages/games.html'>Games</a></li>" +
-    "  <li><a href='../pages/contact.html'>Contact</a></li>" +
-    "</ul>"
-}
