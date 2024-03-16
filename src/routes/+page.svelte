ec145641b53264e13e9f7799ea99fde9d0e02ed7
caveat-dev <nathan.wignall06@gmail.com>
2024-03-15 22:16:08 -0500

    Landing page speedrun

---
+++ src/routes/+page.svelte
@@ -0,0 +1,46 @@
+<body id="body">
+    <div id="title">
+        <h1>Caveat is a hobbyist programmer.</h1>
+    </div>
+    <div id="content">
+        <hr>
+        <h2>Git Me:</h2>
+        <p>Github: <a href="https://github.com/caveat-dev" rel="me">github.com/caveat-dev</a></p>
+        <p>Codeberg: <a href="https://codeberg.org/caveat">codeberg.org/caveat</a></p>
+        <h2>Contact:</h2>
+        <p>Email: <a href="mailto:me@example.com" rel="me">caveat-dev@proton.me</a></p>
+        <p>Discord: <a href="https://discord.com/users/824400229645418497">caveat__</a></p>
+        <hr>
+    </div>
+</body>
+
+<style>
+    #body {
+        background: #232634;
+    }
+    #title {
+        text-align: center;
+        color: #c6d0f5;
+    }
+    #content {
+        text-align: center;
+        background: #303446;
+        color: #c6d0f5;
+    }
+    a:link {
+        color: #8caaee;
+        text-decoration: none;
+    }
+    a:visited {
+        color: #8caaee;
+        text-decoration: none;
+    }
+    a:hover {
+        color: #b5bfe2;
+        text-decoration: none;
+    }
+    a:active {
+        color: #949cbb;
+        text-decoration: none;
+    }
+</style>
