4d4afccd258b5a1630308cb2c7f0d825a9e22511
caveat <cv@caveat.cc>
2024-04-21 02:13:53 -0500

    pretty-ify

---
+++ docs/_app/immutable/entry/start.JzbrZ7H7.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.87Rp0xhA.js";export{t as start};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,27 @@
+<script>
+    let icon_src = "/favicon.png";
+</script>
+
+<title>caveat</title>
 <body id="body">
-    <div id="title">
-        <h1>Caveat is a hobbyist programmer.</h1>
-    </div>
     <div id="content">
         <hr>
         <h2>Git Me:</h2>
         <p>Github: <a href="https://github.com/caveat-dev" rel="me" target="_blank">github.com/caveat-dev</a></p>
         <p>Codeberg: <a href="https://codeberg.org/caveat" target="_blank">codeberg.org/caveat</a></p>
-        <a href="/public_key.txt" target="_blank">Public Key</a>
         <h2>Contact:</h2>
         <p>Email: <a href="mailto:cv@caveat.cc" rel="me" target="_blank">cv@caveat.cc</a></p>
         <p>Discord: <a href="https://discord.com/users/824400229645418497" target="_blank">caveat__</a></p>
+        <h2>Other:</h2>
+        <a href="/public_key.txt" target="_blank">Public Key</a>
         <hr>
     </div>
     <div class="decsinc">
-        <a href="https://roxwize.xyz/ext/decsinc/prev.html?id=1"><h1>––</h1></a>
+        <h1><a href="https://roxwize.xyz/ext/decsinc/prev.html?id=1">––</a></h1>
         <a href="https://roxwize.xyz/site/decsinc.html">
-            <img src="/decsinc_icon.png" alt="decsinc" width="35px"/>
+            <img src="{icon_src}" alt="decsinc" width="35px" on:mouseenter={()=>{icon_src = "/alticon.png"}} on:mouseleave={()=>{icon_src = "/favicon.png";}}/>
         </a>
-        <a href="https://roxwize.xyz/ext/decsinc/next.html?id=1"><h1>++</h1></a>
+        <h1><a href="https://roxwize.xyz/ext/decsinc/next.html?id=1">++</a></h1>
     </div>
 </body>
 
