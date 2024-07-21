21ba2be71826bd983acb7e7c8aa111de9d68f420
caveat <cv@caveat.cc>
2024-07-21 13:50:00 -0500

    convert binaries array to Map

--- src/lib/cash/bin.ts
+++ src/lib/cash/bin.ts
@@ -10,6 +10,19 @@ export type Binary = {
     callback: (args: string[], input: string | null, history: string | null) => ExecutionResult
 }
 
+function createBinary(aliases: string[], description: string, usage: string, callback: (args: string[], input: string | null, history: string | null) => ExecutionResult): Binary {
+    return {
+        aliases: aliases,
+        description: description,
+        usage: usage,
+        callback: callback,
+    }
+}
+
+function createMappedBinary(aliases: string[], description: string, usage: string, callback: (args: string[], input: string | null, history: string | null) => ExecutionResult): [string, Binary] {
+    return [aliases[0], createBinary(aliases, description, usage, callback)];
+}
+
 export type ExecutionResult = {
     result: boolean;
     text: string | null;
@@ -17,14 +30,14 @@ export type ExecutionResult = {
     newHistory: string | null;
 }
 
-export const binaries: Binary[] = [
-    {
-        aliases: ["welcome"],
-        description: "Displays the welcome message.",
-        usage: "welcome",
-        callback: (args) => {
-            if (args.length !== 0) { return returnInvalidArgs(binaries[0]); }
-            let welcomeMessage = `Welcome to https://caveat.cc!\nType [${binaries[1].aliases}] for a list of commands.`;
+export const binaries: Map<string, Binary> = new Map([
+    createMappedBinary(
+        ["welcome"],
+        "Displays the welcome message.",
+        "welcome",
+        (args): ExecutionResult => {
+            if (args.length !== 0) { return returnInvalidArgs(binaries.get("welcome")!); }
+            let welcomeMessage = `Welcome to https://caveat.cc!\nType [${binaries.get("help")!.aliases}] for a list of commands.`;
             return {
                 result: true,
                 text: parse(welcomeMessage),
@@ -32,32 +45,33 @@ export const binaries: Binary[] = [
                 newHistory: null
             };
         }
-    },
-    {
-        aliases: ["help", "h"],
-        description: "Displays available commands.",
-        usage: `help, help ${leftAngleBracket}#page${rightAngleBracket}`,
-        callback: (args) => {
+    ),
+    createMappedBinary(
+        ["help", "h"],
+        "Displays available commands.",
+        `help, help ${leftAngleBracket}#page${rightAngleBracket}`,
+        (args) => {
             let page = 1;
             if (args.length > 1) {
-                return returnInvalidArgs(binaries[1]);
+                return returnInvalidArgs(binaries.get("help")!);
             }
             else if (args.length != 0) {
                 page = Number.parseInt(args[0]);
                 if (isNaN(page)) {
-                    return returnInvalidArgs(binaries[1]);
+                    return returnInvalidArgs(binaries.get("help")!);
                 }
                 page = Math.max(1, page);
-                page = Math.min(Math.ceil(binaries.length / 3), page)
+                page = Math.min(Math.ceil(binaries.size / 3), page)
             }
 
             let lowerBound = (page * 3) - 3;
-            let upperBound = Math.min(lowerBound + 2, binaries.length-1);
+            let upperBound = Math.min(lowerBound + 2, binaries.size-1);
             let toParse = "";
+            let binariesArr = Array.from(binaries);
             for (let i = lowerBound; i <= upperBound; i++) {
-                toParse += `[${binaries[i].aliases}]\nDescription: ${binaries[i].description}\nUsage: ${binaries[i].usage}\n`;
+                toParse += `[${binariesArr[i][1].aliases}]\nDescription: ${binariesArr[i][1].description}\nUsage: ${binariesArr[i][1].usage}\n`;
             }
-            toParse += `Page ${page}/${Math.ceil(binaries.length / 3)}`
+            toParse += `Page ${page}/${Math.ceil(binaries.size / 3)}`
 
             return {
                 result: true,
@@ -66,27 +80,27 @@ export const binaries: Binary[] = [
                 newHistory: null
             };
         }
-    },
-    {
-      aliases: ["clear", "cls"],
-      description: "Clears the screen.",
-      usage: "cls",
-      callback: (args): ExecutionResult => {
-          if (args.length !== 0) { return returnInvalidArgs(binaries[2]); }
-          return {
-              result: true,
-              text: null,
-              historyChanged: true,
-              newHistory: ""
-          };
-      }
-    },
-    {
-        aliases: ["accounts"],
-        description: "Displays Caveat's git pages, socials and other accounts online.",
-        usage: "accounts",
-        callback: (args): ExecutionResult => {
-            if (args.length !== 0) { return returnInvalidArgs(binaries[3]); }
+    ),
+    createMappedBinary(
+        ["clear", "cls"],
+        "Clears the screen.",
+        "cls",
+        (args): ExecutionResult => {
+            if (args.length !== 0) { return returnInvalidArgs(binaries.get("clear")!); }
+            return {
+                result: true,
+                text: null,
+                historyChanged: true,
+                newHistory: ""
+            };
+        }
+    ),
+    createMappedBinary(
+        ["accounts"],
+        "Displays Caveat's git pages, socials, and other accounts online.",
+        "accounts",
+        (args): ExecutionResult => {
+            if (args.length !== 0) { return returnInvalidArgs(binaries.get("accounts")!); }
             let toParse = "Email: cv@caveat.cc\nDiscord: caveat__\nGit+Accounts:\nhttps://codeberg.org/caveat (preferred)\nhttps://github.com/caveat-dev"
             return {
                 result: true,
@@ -95,13 +109,13 @@ export const binaries: Binary[] = [
                 newHistory: null,
             }
         }
-    },
-    {
-        aliases: ["bio"],
-        description: "Displays Caveat's bio.",
-        usage: "bio",
-        callback: (args): ExecutionResult => {
-            if (args.length !== 0) { return returnInvalidArgs(binaries[4]); }
+    ),
+    createMappedBinary(
+        ["bio"],
+        "Displays Caveat's bio.",
+        "bio",
+        (args): ExecutionResult => {
+            if (args.length !== 0) { return returnInvalidArgs(binaries.get("bio")!); }
             let toParse = "Caveat (often stylized as caveat, full name Caveat Emptor) is an amateur developer and FOSS enthusiast from North America.\nThey enjoy developing programs in Rust and Haskell, but begrudgingly use Javascript the most.\nWatch them develop open-source software that nobody uses!"
             return {
                 result: true,
@@ -110,8 +124,8 @@ export const binaries: Binary[] = [
                 newHistory: null,
             }
         }
-    },
-]
+    ),
+]);
 
 export function execute(str: string, input: string, history: string): ExecutionResult {
     let words = str.split(" ");
